7c98d1332edf3eaea2770caf79218baeec3cfcc4
Saleha517 <salehausman517@gmail.com>
2025-08-13 08:49:05 -0700

    Source_Code are Here

---
+++ Class_01/Class_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Saleha's Tech studio");
+            Console.WriteLine("Learn C# With Saleha");
+
+            
+
+            //Console.Write("Welcome to Saleha's Tech studio"); Used for appearnace of data in one line i does ot break the line
+            //Console.WriteLine("Learn C# With Saleha"); Used for appereance of Data in Command Line and it breaks the line 
+            //Console.ReadKey(); used for Read Data in CMD
+            //Console.ReadLine used for get data from user
+            // \" used for Double quotes
+            // \' used for Sigle Quotes
+            // \n used for line break 
+            // \t used foe one tab space
+             
+
+
+            Console.ReadKey();
+        }
+    }
+}
